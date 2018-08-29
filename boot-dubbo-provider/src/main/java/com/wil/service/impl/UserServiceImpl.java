643be7a001d5fe4a92821734eef3f28734d3d9cc
wilbai <1058191407@qq.com>
2018-08-29 14:50:54 +0800

    SpringBoot+Dubbo,生产者配置

--- boot-dubbo-provider/src/main/java/com/wil/service/impl/UserServiceImpl.java
+++ boot-dubbo-provider/src/main/java/com/wil/service/impl/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.wil.service.impl;
 
 import com.wil.service.UserService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by wil on 2017/12/11.
  */
+@Service
 public class UserServiceImpl implements UserService {
     @Override
     public void sayHello(String name) {
